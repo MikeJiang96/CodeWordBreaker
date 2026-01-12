b03953d797bc4df0aee19123e343a54b746b27b3
Mike Jiang <jyt19960208@gmail.com>
2026-01-12 23:51:11 +0800

    Initial Commit

---
+++ CodeWordBreaker/CodeWordBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeWordBreakerApp.swift
+//  CodeWordBreaker
+//
+//  Created by Mike Jiang on 2026/1/12.
+//
+
+import SwiftUI
+
+@main
+struct CodeWordBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeWordBreaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeWordBreaker
+//
+//  Created by Mike Jiang on 2026/1/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
